5f97e1eed9745d0c5c489c364c2bc07f225dbbec
Kelven Bento Fontes <kelvenbentofontes@gmail.com>
2025-03-13 21:47:22 -0300

    feat: Remove unnecessary files from your project and simplify Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  distDir: "docs",
-  basePath: "/my-portfolio",
-  assertsPrefix: "/my-portfolio",
-  images: {
-    unoptimized: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
