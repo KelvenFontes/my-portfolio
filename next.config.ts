846cfca12053e0d44e7ab4a1c9d3295141741bb5
Kelven Bento Fontes <kelvenbentofontes@gmail.com>
2025-03-13 21:23:16 -0300

    feat: Deploy do Next.js para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "docs",
+  // basePath: "/SEU_REPOSITORIO",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
