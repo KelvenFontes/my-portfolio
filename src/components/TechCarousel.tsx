f9d5052eb2bbb0b94ad07f3fdbe790839eb146d1
Kelven Bento Fontes <kelvenbentofontes@gmail.com>
2025-03-15 02:21:36 -0300

    refactor: adjust margins and remove unnecessary class in TechCarousel component

--- src/components/TechCarousel.tsx
+++ src/components/TechCarousel.tsx
@@ -41,14 +41,14 @@ export default function TechCarousel() {
 
     return (
         <div className="max-w-sm md:max-w-xl lg:max-w-5xl mx-auto py-10 px-4 text-center">
-            <h2 className="text-2xl md:text-3xl font-bold text-white mb-6">💻 Minhas Tecnologias</h2>
-            <Slider {...settings} className="text-white w-full">
+            <h2 className="text-2xl md:text-3xl font-bold mb-12">💻 Minhas Tecnologias</h2>
+            <Slider {...settings} className="w-full">
                 {techs.map((tech, index) => (
                     <div key={index} className="flex flex-col items-center justify-center h-24 md:h-32">
                         <div className="flex flex-col items-center justify-center">
                             {tech.icon}
                         </div>
-                        <p className="text-gray-300 text-xs md:text-sm mt-2">{tech.name}</p>
+                        <p className="text-xs md:text-sm mt-2">{tech.name}</p>
                     </div>
                 ))}
             </Slider>
