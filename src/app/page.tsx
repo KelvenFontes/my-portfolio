f55aa73ca29b90bd97dd6e142f73b0ff201f38f4
Kelven Bento Fontes <kelvenbentofontes@gmail.com>
2026-03-08 19:32:05 -0300

    refactor: remove unused imports and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,13 @@ import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import ScrollToTop from "../components/ScrollToTop";
-// import PaymentsExperience from "../components/PaymentsExperience";
-// import TechCarousel from "../components/TechCarousel";
 import Hero from "@/components/Hero";
-// import { BackgroundBeams } from "@/components/ui/background-beams";
-// import { Spotlight } from "@/components/ui/Spotligh";
 import { CodeBlock } from "@/components/CodeBlock";
-// import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
-// import { Experience } from "@/components/Experience";
-
-
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center mx-auto">
       <Navbar />
-
-      {/* <Spotlight /> */}
-      {/* <BackgroundBeams /> */}
 
       <motion.section id="Hero" className="w-full max-w-4xl px-6 md:px-12 py-16 mx-auto text-center flex flex-col items-center justify-center" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.8 }}>
         <Hero />
@@ -54,38 +43,14 @@ export default function Home() {
       </div>
 
 
-      {/* <motion.section id="services" className="w-full max-w-4xl px-6 md:px-12 py-16 mx-auto text-center flex flex-col items-center justify-center" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-        <Services />
-      </motion.section> */}
-
-      {/* <motion.section id="technologies" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-        <TechCarousel />
-      </motion.section> */}
-
-      {/* <motion.section id="payments" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-        <PaymentsExperience />
-      </motion.section> */}
-
-
-
       <motion.section id="projects" className="w-full max-w-4xl px-6 md:px-12 py-16 mx-auto text-center flex flex-col items-center justify-center" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
         <Projects />
       </motion.section>
 
-      {/* <motion.section id="blog" className="w-full max-w-4xl px-6 md:px-12 py-16 mx-auto text-center flex flex-col items-center justify-center" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-        <Blog />
-      </motion.section> */}
-
-      {/* <Stats /> */}
-
-      {/* <Experience /> */}
-
       <div className="flex items-center justify-center">
-        {/* <BackgroundBeamsWithCollision> */}
         <motion.section id="contact" className="w-full max-w-4xl px-6 md:px-12 mb-16 mx-auto text-center flex flex-col items-center justify-center" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
           <Contact />
         </motion.section>
-        {/* </BackgroundBeamsWithCollision> */}
       </div>
 
       <Footer />
